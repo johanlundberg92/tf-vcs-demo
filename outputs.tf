b0470a1825acf11d90b873aee6c9a43af6cba823
johanlundberg92 <johan.lundberg1992@gmail.com>
2024-01-10 11:14:44 +0100

    initial code

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "random_name_1" {
+    value = random_id.server1.b64_std
+}
+output "random_name_2" {
+    value = random_id.server2.b64_std
+}
---
+++ random.tf
@@ -0,0 +1,22 @@
+locals {
+  time_string = "${timestamp()}"
+}
+
+resource "random_id" "server1" {
+    keepers = {
+        name = local.time_string
+    }
+    byte_length = 12
+}
+resource "random_id" "server2" {
+    keepers = {
+        name = local.time_string
+    }
+    byte_length = 12
+    prefix = "server-"
+}
+resource "terraform_data" "nothing" {
+  provisioner "local-exec" {
+    command = "echo 'Hello world ${random_id.server1.id}!'"
+  }
+}
